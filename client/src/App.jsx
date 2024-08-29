de3a5358c6794ec35c121de5b388a93a8f007984
unknown <ahmdsmyr812@gmail.com>
2024-08-30 02:47:34 +0300

    create the header section and update router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import DashboardPage from "./pages/DashboardPage";
-import SigninPage from "./pages/SignInPage";
-import SignUpPage from "./pages/SignUpPage ";
-import Projectspage from "./pages/Projectspage";
-import NotFoundPage from "./pages/NotFoundPage";
+import { BrowserRouter } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import MainRoutes from "./MainRoutes";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/singin" element={<SigninPage />} />
-        <Route path="/singup" element={<SignUpPage />} />
-        <Route path="/projects" element={<Projectspage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <MainHeader />
+      <MainRoutes />
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/MainRoutes.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import DashboardPage from "./pages/DashboardPage";
+import SigninPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage ";
+import Projectspage from "./pages/Projectspage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/singin" element={<SigninPage />} />
+      <Route path="/singup" element={<SignUpPage />} />
+      <Route path="/projects" element={<Projectspage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
+
+export default MainRoutes;
