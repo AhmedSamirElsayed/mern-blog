f7075899468b276c470b893ab0bc6463c00ad6ca
unknown <ahmdsmyr812@gmail.com>
2024-08-31 01:54:48 +0300

    create the  footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import MainRoutes from "./MainRoutes";
+import MainFooter from "./components/MainFooter";
 
 function App() {
   return (
     <BrowserRouter>
       <MainHeader />
       <MainRoutes />
+      <MainFooter />
     </BrowserRouter>
   );
 }
