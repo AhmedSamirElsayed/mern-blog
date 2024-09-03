a8d604466b4efb16de1de14c7da7fc5a8797cde3
unknown <ahmdsmyr812@gmail.com>
2024-09-03 14:12:55 +0300

    creat sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandelar } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -28,3 +29,75 @@ export const signup = async (req, res, next) => {
     next(error); //use midleWare
   }
 };
+
+// // handle log in controller
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandelar(400, "All fields are required"));
+  }
+
+  try {
+    // 1- check validate of email
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandelar(404, "User not found"));
+    }
+    // 2- check validate of password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandelar(400, "Invalid email or password"));
+    }
+
+    // if user valid generate token to send it with respons in cookie . to auth user
+    const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRT);
+    // Exclude the password from the response
+    const { password: pass, ...rest } = validUser._doc;
+
+    // console.log(validUser);
+    // console.log("------------------------------------");
+    // console.log(rest);
+    return (
+      res
+        .status(200)
+        .cookie("access_token", token, { httpOnly: true })
+        // .json(validUser);
+        .json(rest)
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+// export const signin = async (req, res, next) => {
+//   const { email, password } = req.body;
+
+//   if (!email || !password || email === "" || password === "") {
+//     return next(errorHandelar(400, "All fields are required"));
+//   }
+
+//   try {
+//     // 1- Check if the email exists
+//     const validUser = await User.findOne({ email });
+//     if (!validUser) {
+//       return next(errorHandelar(404, "User not found"));
+//     }
+
+//     // 2- Check if the password is correct
+//     const validPassword = bcryptjs.compareSync(password, validUser.password);
+//     if (!validPassword) {
+//       return next(errorHandelar(400, "Invalid email or password"));
+//     }
+
+//     // If the user is valid, generate a token and send it in the response
+//     const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRT);
+//     const { password: pass, ...rest } = validUser._doc;
+//     return res
+//       .status(200)
+//       .cookie("access_token", token, { httpOnly: true })
+//       .json(rest);
+//   } catch (error) {
+//     return next(error);
+//   }
+// };
