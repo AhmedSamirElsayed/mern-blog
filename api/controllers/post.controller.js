8478edb62e2935dc2f9a17a1fab93ddaa3d2c88b
unknown <ahmdsmyr812@gmail.com>
2024-09-12 00:48:51 +0300

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -37,9 +37,17 @@ export const getALLposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sorteDirection = req.query.order === "asc" ? 1 : -1;
-    const posts = await Post.find({
-      ...(req.query.userId && { userId: req.query.userId }),
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+    const Now = new Date();
+    const oneMonthAgo = new Date(
+      Now.getFullYear(),
+      Now.getMonth() - 1,
+      Now.getDate()
+    );
+
+    const filters = {
+      ...(req.query.userId && { _id: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
       ...(req.query.slug && { slug: req.query.slug }),
       ...(req.query.postId && { _id: req.query.postId }),
@@ -49,19 +57,13 @@ export const getALLposts = async (req, res, next) => {
           { content: { $regex: req.query.searchTerm, $options: "i" } },
         ],
       }),
-    })
-      .sort({ updateAt: sorteDirection })
+    };
+
+    const posts = await Post.find(filters)
+      .sort({ updatedAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
-
     const totalPosts = await Post.countDocuments();
-    const Now = new Date();
-    const oneMonthAgo = new Date(
-      Now.getFullYear(),
-      Now.getMonth(),
-      Now.getDate()
-    );
-
     const lastMonthPosts = await Post.countDocuments({
       createdAt: { $gte: oneMonthAgo },
     });
--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 const DashPosts = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userposts, setUserPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?_id=${currentUser._id}`);
+        const data = await res.json();
+
+        if (res.ok) {
+          setUserPosts(data.posts);
+        } else {
+          throw new Error(data.message || "Failed to fetch posts");
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    if (currentUser?.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser]);
+
   return (
     <div className="h-screen bg-yellow-100 text-center w-full text-black">
-      DashPost
+      {error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <>
+          <h1>DashPosts</h1>
+          {userposts.length > 0 ? (
+            <ul>
+              {userposts.map((post) => (
+                <>
+                  <li key={post._id}>{post.title}</li>
+                  <li>{post.content}</li>
+                </>
+              ))}
+            </ul>
+          ) : (
+            <p>No posts available</p>
+          )}
+        </>
+      )}
     </div>
   );
 };
