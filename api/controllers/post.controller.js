0588237abb229ae158c394efbd5b73331a5afde0
unknown <ahmdsmyr812@gmail.com>
2024-09-11 19:31:13 +0300

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import Post from "../models/post.model.js";
 import { errorHandelar } from "../utils/error.js";
 
+// creat post
 export const create = async (req, res, next) => {
   //   console.log(req.user);
 
@@ -26,6 +27,50 @@ export const create = async (req, res, next) => {
   try {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// get ALL posts
+export const getALLposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sorteDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updateAt: sorteDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+    const Now = new Date();
+    const oneMonthAgo = new Date(
+      Now.getFullYear(),
+      Now.getMonth(),
+      Now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
   } catch (error) {
     next(error);
   }
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { verifyUserToken } from "../utils/processOnUserToken.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getALLposts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 router.post("/create", verifyUserToken, create);
+router.get("/getposts", getALLposts);
 
 export default router;
